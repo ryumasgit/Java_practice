9ef32fd91211ee92e29229c256747b38364fe137
ryumasgit <135499359+ryumasgit@users.noreply.github.com>
2023-11-16 13:22:58 +0000

    add

--- practice-1/FizzBuzz.java
+++ practice-1/FizzBuzz.java
@@ -1,10 +1,11 @@
 public class FizzBuzz {
     public static void main(String[] args) {
-        for ( int i = 1; i <= 30; i++ ) {
-            if ( i % 3 == 0 && i % 5 == 0 ) System.out.print("FizzBuzz");
-            else if ( i % 3 == 0 ) System.out.print("Fizz");
-            else if ( i % 5 == 0 ) System.out.print("Buzz");
-            else System.out.print(i);
-        }
+        fizzBuzz(15);
+    }
+    public static void fizzBuzz(Integer value) {
+        if ( value % 15 == 0 ) System.out.print("FizzBuzz");
+        else if ( value % 3 == 0 ) System.out.print("Fizz");
+        else if ( value % 5 == 0 ) System.out.print("Buzz");
+        else System.out.print(value);
     }
 }
--- practice-1/HelloJava.java
+++ practice-1/HelloJava.java
@@ -1,4 +1,4 @@
-class HelloJava {
+public class HelloJava {
     public static void main(String[] args) {
         System.out.println("Hello Java");
     }
