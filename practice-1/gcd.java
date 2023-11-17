b30e9f612b24fdaa56b6dc2c23dba49a50085563
ryumasgit <135499359+ryumasgit@users.noreply.github.com>
2023-11-17 14:56:04 +0000

    add

---
+++ practice-1/gcd.java
@@ -0,0 +1,27 @@
+// 問題1: 最大公約数 (GCD) の計算
+// 最大公約数（Greatest Common Divisor, GCD）を求めるプログラムを書いてください。2つの整数を入力とし、それらの最大公約数を返す関数を実装してください。
+
+// def GCD(target1, target2):
+//     divior = []
+//     min_num = min(target1, target2)
+//     for i in range(2,min_num + 1):
+//         if target1 % i == 0 and target2 % i == 0:
+//             divior.append(i)
+//         else:
+//             return "公約数はありません"
+//     return max(divior)
+
+// target1 = 3
+// target2 = 2
+// print(GCD(target1, target2))
+
+class gcd {
+  public static void main(String[] agrs) {
+    int target1 = 3;
+    int target2 ~ 2;
+    System.out.println(gcd(target1, target2));
+  }
+  public static int calcuration_gcd(int target1, int target2) {
+    int[] 
+  }
+}
