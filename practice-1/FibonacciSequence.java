16baeb73059b62e9f7ac262f4c3f16269ff44d79
ryumasgit <135499359+ryumasgit@users.noreply.github.com>
2023-11-18 15:16:16 +0000

    add

--- practice-1/FibonacciSequence.java
+++ practice-1/FibonacciSequence.java
@@ -1,16 +1,28 @@
-// def fibonacci_sequence(target):
-//     if target < 0:
-//         return "負の数は指定できません"
-//     elif target == 0:
-//         return 0
-//     elif target == 1:
-//         return 1
-//     else:
-//         num1 = 0
-//         num2 = 1
+public class FibonacciSequence {
+  public static void main(String[] args) {
+    int target = 5;
+    if (target >= 0) {
+      int result = calcurationFibonacciSequence(target);
+      System.out.println(result);
+    }else {
+      System.out.println("負の数は指定できません");
+    }
+  }
+  public static int calcurationFibonacciSequence(int target) {
+    if (target == 0) {
+      return 0;
+    }else if (target == 1) {
+      return 1;
+    }else {
+      int num1 = 0;
+      int num2 = 1;
 
-//         for i in range(3,target + 1):
-//           num1, num2 = num2, num1 + num2
-//         return num2
-
-// print(fibonacci_sequence(5))
+      for (int i = 3; i < target + 1; i ++) {
+        int tmp = num1;
+        num1 = num2;
+        num2 = tmp + num2;
+      }
+      return num2;
+    }
+  }
+}
