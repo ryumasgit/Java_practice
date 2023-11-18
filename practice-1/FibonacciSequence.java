2dd1858ad22d8aef45f937e056c3c2c6a9b4695d
ryumasgit <135499359+ryumasgit@users.noreply.github.com>
2023-11-18 14:56:22 +0000

    add

---
+++ practice-1/FibonacciSequence.java
@@ -0,0 +1,16 @@
+// def fibonacci_sequence(target):
+//     if target < 0:
+//         return "負の数は指定できません"
+//     elif target == 0:
+//         return 0
+//     elif target == 1:
+//         return 1
+//     else:
+//         num1 = 0
+//         num2 = 1
+
+//         for i in range(3,target + 1):
+//           num1, num2 = num2, num1 + num2
+//         return num2
+
+// print(fibonacci_sequence(5))
